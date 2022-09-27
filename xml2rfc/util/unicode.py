3998d8e1b142ca76d706b80ad6b7afe343fe8425
Kesara Rathnayake <kesara@fq.nz>
2022-09-27 17:53:04 +1300

    feat: Allow Unicode in t element
    This change allows bare Unicode in t element.
    This also introduces new flag `--warn-bare-unicode` when set, `xml2rfc`
    warns about bare Unicode in `<t>` element.
    By default this is set to False.

--- xml2rfc/util/unicode.py
+++ xml2rfc/util/unicode.py
@@ -34,6 +34,7 @@ unicode_content_tags = set([
     'street',
     'title',
     'u',
+    't',
 ])
 
 # Attribute values should not contain unicode, with some exceptions
@@ -83,6 +84,9 @@ bare_latin_tags = set([
     'title',
     ])
 
+bare_unicode_tags_with_notice = set([
+    't',
+])
 
 def is_svg(e):
     '''
@@ -93,10 +97,6 @@ def is_svg(e):
         return True
     else:
         return False
-
-def enable_rfc_mode_rfc7997_treatment():
-    bare_unicode_tags.update(['t'])
-    unicode_content_tags.update(['t'])
 
 for t in bare_unicode_tags:
     assert t in unicode_content_tags
